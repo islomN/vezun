3edffaa41b1121770946b3c96715925961437f0e
Islom <nurmanovig@gmail.com>
2019-02-25 15:44:36 +0500

    change telegram hook

--- frontend/components/TelegramHook.php
+++ frontend/components/TelegramHook.php
@@ -11,7 +11,6 @@ class TelegramHook{
         $res = $this->telegram->setWebhook(['url' => \Yii::getAlias("@server")]);
 
         print_r($this->telegram->hook());
-        file_put_contents(\Yii::getAlias("@frontend/web/")."hook.log", print_r($this->telegram->hook(), false));
         print_r($this->telegram->getChat(['chat_id' => '749493022']));
     }
 
