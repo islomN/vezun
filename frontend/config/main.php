a0642e4e4aba7242ad36993f5fbea84c246dcfbb
Islom <nurmanovig@gmail.com>
2019-02-25 15:52:32 +0500

    add htaccess rules

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -19,6 +19,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'baseUrl' => ''
         ],
         'user' => [
             'identityClass' => 'common\models\User',
